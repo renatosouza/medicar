d13bdb8185f6d6ca84eda18061cf0af99ebd3a0a
Renato <renatosouzavv@gmail.com>
2020-06-08 17:13:49 -0300

    Criação dos modelos

--- medicar_server/api/models.py
+++ medicar_server/api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Especialidade(models.Model):
+    nome = models.CharField(max_length=40)
+
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=40)
+    crm = models.IntegerField()
+    email = models.EmailField(blank=True)
+    telefone = models.CharField(max_length=20, blank=True)
+    especialidade = models.ForeignKey(Especialidade, 
+                                      related_name='medicos', 
+                                      on_delete=models.CASCADE)    
+
+
+class Agenda(models.Model):
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    data = models.DateField()
+
+
+class Horario(models.Model):
+    horario = models.TimeField()
+    agenda = models.ForeignKey(Agenda, 
+                               related_name='horarios', 
+                               on_delete=models.CASCADE)
+
+
+class Consulta(models.Model):
+    data = models.DateField()
+    horario = models.ForeignKey(Horario, on_delete=models.CASCADE)
+    data_agendamento = models.DateTimeField(auto_now_add=True)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    cliente = models.ForeignKey('auth.User', 
+                                related_name='consultas', 
+                                on_delete=models.CASCADE)
