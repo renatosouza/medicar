8860f996c23c03beb5beac728fd9f86bc2a66dc3
Renato <renatosouzavv@gmail.com>
2020-06-15 16:21:14 -0300

    Adiciona o componente de header pra homepage (estático)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @HostBinding('attr.class') cssClass = 'app';
   title = 'frontend';
 }
