421850b89a28e763c2ab7178358a8dde341a2210
Renato <renatosouzavv@gmail.com>
2020-06-16 00:04:30 -0300

    Adiciona componente marcar consulta incompleto (estático)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListaConsultasComponent } from './lista-consultas/lista-consultas.component';
+import { NovaConsultaComponent } from './nova-consulta/nova-consulta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListaConsultasComponent
+    ListaConsultasComponent,
+    NovaConsultaComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/nova-consulta/nova-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-consulta',
+  templateUrl: './nova-consulta.component.html',
+  styleUrls: ['./nova-consulta.component.css']
+})
+export class NovaConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
