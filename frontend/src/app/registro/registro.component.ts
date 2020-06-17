83f5517435b62f3a4e6ffa0790a050b6c14a5e86
Renato <renatosouzavv@gmail.com>
2020-06-17 18:06:43 -0300

    Aplica Reactive Forms ao componente registro

--- frontend/src/app/registro/registro.component.ts
+++ frontend/src/app/registro/registro.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+
+function confirmaSenhaValidation(group: FormGroup): {[s: string]: boolean} {
+  if((!group) || (!group.get('password')) || (!group.get('password2'))) {
+    return null;
+  }
+  let password = group.get('password').value;
+  let password2 = group.get('password2').value;
+
+  return password === password2 ? null : { notSame: true } 
+}
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +18,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) { 
+    this.form = fb.group({
+      'username': [null, Validators.required],
+      'email': [null, Validators.required],
+      'password': [null, Validators.required],
+      'password2': [null, Validators.required]
+    }, { validator: confirmaSenhaValidation });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
   }
 
 }
