53daafe5f6111ea444204e732ccb6b8490a19d0c
Renato <renatosouzavv@gmail.com>
2020-06-17 15:51:15 -0300

    Aplica Reactive Forms ao componente login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) { 
+    this.form = fb.group({
+      'username': ['', Validators.required],
+      'password': ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
   }
 
 }
